c47c73fb0437b784f1b51bc415183d91067f6d96
chasomin <somin49@naver.com>
2024-02-27 20:01:37 +0900

    UIFont Extension 생성

---
+++ CryptoCoinApp/Extension/UIFont+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+Extension.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 2/27/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let largeTitle = UIFont.systemFont(ofSize: 25, weight: .heavy)
+    static let title = UIFont.systemFont(ofSize: 20, weight: .heavy)
+    static let body = UIFont.systemFont(ofSize: 17)
+    static let boldBody = UIFont.systemFont(ofSize: 17, weight: .bold)
+    static let caption = UIFont.systemFont(ofSize: 14)
+    static let boldCaption = UIFont.systemFont(ofSize: 14, weight: .bold)
+}
