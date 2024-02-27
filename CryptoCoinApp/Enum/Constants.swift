6e086c8b19fc453d24a5315921403cf19db9a8e5
chasomin <somin49@naver.com>
2024-02-28 00:04:34 +0900

    Constants enum 추가 (navigationTitle)

---
+++ CryptoCoinApp/Enum/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 2/27/24.
+//
+
+import Foundation
+
+enum Constants {
+    enum NavigationTitle: String {
+        case cryptoCoin = "Crypto Coin"
+        case search = "Search"
+        case favorite = "Favorite Coin"
+    }
+}
