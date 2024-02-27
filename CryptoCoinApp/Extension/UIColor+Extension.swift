93516febdb5f51b2a514ec8e5afd56e0444ad93d
chasomin <somin49@naver.com>
2024-02-27 15:31:48 +0900

    Set Custom Color

---
+++ CryptoCoinApp/Extension/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 2/27/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let titleColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    static let backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+    static let secondaryBackgroundColor = UIColor(red: 243, green: 244, blue: 246, alpha: 1)
+    static let labelColor = UIColor(red: 52, green: 61, blue: 76, alpha: 1)
+    static let secondaryLabelColor = UIColor(red: 130, green: 130, blue: 130, alpha: 1)
+    
+    static let pointColor = UIColor(red: 145, green: 76, blue: 245, alpha: 1)
+    static let upperLabelColor = UIColor(red: 240, green: 68, blue: 82, alpha: 1)
+    static let upperBackColor = UIColor(red: 255, green: 234, blue: 237, alpha: 1)
+    static let lowerLabelColor = UIColor(red: 50, green: 130, blue: 248, alpha: 1)
+    static let lowerBackColor = UIColor(red: 229, green: 240, blue: 255, alpha: 1)
+}
