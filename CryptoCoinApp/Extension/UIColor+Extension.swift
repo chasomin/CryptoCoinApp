56a127df3add68459c8392be41fdf59568771175
chasomin <somin49@naver.com>
2024-02-27 20:01:18 +0900

    UIColor Extension 수정

--- CryptoCoinApp/Extension/UIColor+Extension.swift
+++ CryptoCoinApp/Extension/UIColor+Extension.swift
@@ -9,14 +9,14 @@ import UIKit
 
 extension UIColor {
     static let titleColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-    static let backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
-    static let secondaryBackgroundColor = UIColor(red: 243, green: 244, blue: 246, alpha: 1)
-    static let labelColor = UIColor(red: 52, green: 61, blue: 76, alpha: 1)
-    static let secondaryLabelColor = UIColor(red: 130, green: 130, blue: 130, alpha: 1)
+    static let backgroundColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 1)
+    static let secondaryBackgroundColor = UIColor(red: 243/255.0, green: 244/255.0, blue: 246/255.0, alpha: 1)
+    static let labelColor = UIColor(red: 52/255.0, green: 61/255.0, blue: 76/255.0, alpha: 1)
+    static let secondaryLabelColor = UIColor(red: 130/255.0, green: 130/255.0, blue: 130/255.0, alpha: 1)
     
-    static let pointColor = UIColor(red: 145, green: 76, blue: 245, alpha: 1)
-    static let upperLabelColor = UIColor(red: 240, green: 68, blue: 82, alpha: 1)
-    static let upperBackColor = UIColor(red: 255, green: 234, blue: 237, alpha: 1)
-    static let lowerLabelColor = UIColor(red: 50, green: 130, blue: 248, alpha: 1)
-    static let lowerBackColor = UIColor(red: 229, green: 240, blue: 255, alpha: 1)
+    static let pointColor = UIColor(red: 145/255.0, green: 76/255.0, blue: 245/255.0, alpha: 1)
+    static let upperLabelColor = UIColor(red: 240/255.0, green: 68/255.0, blue: 82/255.0, alpha: 1)
+    static let upperBackColor = UIColor(red: 255/255.0, green: 234/255.0, blue: 237/255.0, alpha: 1)
+    static let lowerLabelColor = UIColor(red: 50/255.0, green: 130/255.0, blue: 248/255.0, alpha: 1)
+    static let lowerBackColor = UIColor(red: 229/255.0, green: 240/255.0, blue: 255/255.0, alpha: 1)
 }
