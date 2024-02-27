1509c0851ecb72c85d1ddace33774764d4c958f8
chasomin <somin49@naver.com>
2024-02-28 00:09:34 +0900

    Coin Model init추가

--- CryptoCoinApp/Network/Model/Coin.swift
+++ CryptoCoinApp/Network/Model/Coin.swift
@@ -19,6 +19,27 @@ struct Item: Decodable {
     let small: String?   // trending에서 쓰이는 이미지
     let data: CoinData?
     let market_cap_rank: Int? // 순위. 정렬가능
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case symbol
+        case thumb
+        case small
+        case data
+        case market_cap_rank
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.symbol = try container.decode(String.self, forKey: .symbol)
+        self.thumb = try container.decode(String.self, forKey: .thumb)
+        self.small = try container.decodeIfPresent(String.self, forKey: .small) ?? ""
+        self.data = try container.decodeIfPresent(CoinData.self, forKey: .data) ?? CoinData(price: "", price_change_percentage_24h: PricePercentage(krw: 0))
+        self.market_cap_rank = try container.decodeIfPresent(Int.self, forKey: .market_cap_rank) ?? 0
+    }
 }
 
 struct CoinData: Decodable {
