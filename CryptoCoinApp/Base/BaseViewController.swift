6aada768908134acdd72ce5fe13fe93b65af22f8
chasomin <somin49@naver.com>
2024-02-27 15:32:47 +0900

    BaseViewController 생성

---
+++ CryptoCoinApp/Base/BaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 2/27/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.backgroundColor
+
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+}
