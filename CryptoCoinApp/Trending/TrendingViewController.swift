fd803522ad29919519c6c7c5f692cd822a172ebf
chasomin <somin49@naver.com>
2024-02-28 01:11:59 +0900

    TrendingViewController 생성

---
+++ CryptoCoinApp/Trending/TrendingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TrendingViewController.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 2/28/24.
+//
+
+import UIKit
+
+class TrendingViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = Constants.NavigationTitle.trending.rawValue
+        
+    }
+
+}
