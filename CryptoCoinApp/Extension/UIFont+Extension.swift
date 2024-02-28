b7b45838f82d710c3fbb0e58cb2a33fb9e99eff4
chasomin <somin49@naver.com>
2024-02-28 20:42:12 +0900

    BoldTitle 폰트 추가

--- CryptoCoinApp/Extension/UIFont+Extension.swift
+++ CryptoCoinApp/Extension/UIFont+Extension.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 extension UIFont {
-    static let largeTitle = UIFont.systemFont(ofSize: 25, weight: .heavy)
-    static let title = UIFont.systemFont(ofSize: 20, weight: .heavy)
+    static let largeTitle = UIFont.systemFont(ofSize: 34, weight: .bold)
+    static let boldTitle = UIFont.systemFont(ofSize: 20, weight: .heavy)
+    static let title = UIFont.systemFont(ofSize: 20)
     static let body = UIFont.systemFont(ofSize: 17)
     static let boldBody = UIFont.systemFont(ofSize: 17, weight: .bold)
     static let caption = UIFont.systemFont(ofSize: 13)
