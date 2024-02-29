442169d99de41993f0ab4ae88e87abd4c51e0e27
chasomin <somin49@naver.com>
2024-03-01 03:58:26 +0900

    Trending 화면 구성

--- CryptoCoinApp/Trending/TrendingViewController.swift
+++ CryptoCoinApp/Trending/TrendingViewController.swift
@@ -6,8 +6,28 @@
 //
 
 import UIKit
+import SnapKit
 
-class TrendingViewController: BaseViewController {
+enum TrendingCell: Int, CaseIterable {
+    case favorite
+    case topCoin
+    case topNFT
+    
+    var title: String {
+        switch self {
+        case .favorite:
+            "My Favorite"
+        case .topCoin:
+            "Top 15 Coin"
+        case .topNFT:
+            "Top 7 NFT"
+        }
+    }
+}
+
+
+final class TrendingViewController: BaseViewController {
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +35,58 @@ class TrendingViewController: BaseViewController {
         navigationItem.title = Constants.NavigationTitle.trending.rawValue
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FavoriteTableViewCell.self, forCellReuseIdentifier: FavoriteTableViewCell.id)
+        tableView.register(RankTableViewCell.self, forCellReuseIdentifier: RankTableViewCell.id)
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 250
+    }
 
+}
+
+extension TrendingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        TrendingCell.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch TrendingCell(rawValue: indexPath.row) {
+        case .favorite :
+            let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteTableViewCell.id, for: indexPath) as! FavoriteTableViewCell
+            return cell
+
+        case .topCoin:
+            let cell = tableView.dequeueReusableCell(withIdentifier: RankTableViewCell.id, for: indexPath) as! RankTableViewCell
+            cell.rankTitleLabel.text = TrendingCell.topCoin.title
+            cell.viewModel.inputWhatKindOfCell.value = indexPath.row
+            return cell
+
+        case .topNFT:
+            let cell = tableView.dequeueReusableCell(withIdentifier: RankTableViewCell.id, for: indexPath) as! RankTableViewCell
+            cell.rankTitleLabel.text = TrendingCell.topNFT.title
+            cell.viewModel.inputWhatKindOfCell.value = indexPath.row
+            return cell
+
+        case .none:
+            return UITableViewCell()
+        }
+    }
 }
