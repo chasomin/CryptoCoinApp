6f728411d6a64a7e69fbc1747bbae932ff12952f
chasomin <somin49@naver.com>
2024-03-03 03:32:42 +0900

    상황에 맞는 custom error 보내주기

--- CryptoCoinApp/Network/APIService.swift
+++ CryptoCoinApp/Network/APIService.swift
@@ -8,45 +8,40 @@
 import Foundation
 import Alamofire
 
-//TODO: ErrorHandling
-enum CoinError: Error {
-    
-}
-
 final class APIService {
     static let shared = APIService()
     private init() { }
     
     //TODO: CompletionHandler 말고 return으로 하게 되면 Repository 에서 처리하기ㅣㅣㅣㅣ~~
-    func fetchTrendingAPI(api: CoinAPI, completionHandler: @escaping (Trending?, Error?) -> Void) {
+    func fetchTrendingAPI(api: CoinAPI, completionHandler: @escaping (Trending?, CryptoCoinError?) -> Void) {
         AF.request(api.endPoint, parameters: api.parameter).responseDecodable(of: TrendingDTO.self) { response in
             switch response.result {
             case .success(let success):
                 completionHandler(success.toEntity(), nil)
             case .failure(let failure):
-                completionHandler(nil, failure)
+                completionHandler(nil, .failNetworking)
             }
         }
     }
     
-    func fetchCoinSearchAPI(api: CoinAPI, completionHandler: @escaping (Search?, Error?) -> Void) {
+    func fetchCoinSearchAPI(api: CoinAPI, completionHandler: @escaping (Search?, CryptoCoinError?) -> Void) {
         AF.request(api.endPoint, parameters: api.parameter).responseDecodable(of: SearchDTO.self) { response in
             switch response.result {
             case .success(let success):
                 completionHandler(success.toEntity(), nil)
             case .failure(let failure):
-                completionHandler(nil, failure)
+                completionHandler(nil, .failNetworking)
             }
         }
     }
     
-    func fetchCoinMarketAPI(api: CoinAPI, completionHandler: @escaping ([Market]?, Error?) -> Void) {
+    func fetchCoinMarketAPI(api: CoinAPI, completionHandler: @escaping ([Market]?, CryptoCoinError?) -> Void) {
         AF.request(api.endPoint, parameters: api.parameter).responseDecodable(of: [MarketDTO].self) { response in
             switch response.result {
             case .success(let success):
                 completionHandler(success.map{$0.toEntity()}, nil)
             case .failure(let failure):
-                completionHandler(nil, failure)
+                completionHandler(nil, .failNetworking)
             }
         }
     }
