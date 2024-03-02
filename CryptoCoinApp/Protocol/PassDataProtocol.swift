a96bd8b95046dbaff025fb0e5c33a88308b08595
chasomin <somin49@naver.com>
2024-03-03 03:24:20 +0900

    버튼 클릭 시 해당 tag 값 전달하는 함수 protocol 생성

---
+++ CryptoCoinApp/Protocol/PassDataProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  PassDataProtocol.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 3/2/24.
+//
+
+import Foundation
+
+protocol PassDataProtocol: AnyObject {
+    func buttonTapped(tag: Int)
+}
