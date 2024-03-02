3e0e5f8c95f267719fccf8cb0174fe2291236bea
chasomin <somin49@naver.com>
2024-03-03 03:24:49 +0900

    셀 선택 시 id 값 전달하는 함수 protocol 생성

---
+++ CryptoCoinApp/Protocol/ViewTransitionProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ViewTransitionProtocol.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 3/2/24.
+//
+
+import Foundation
+
+protocol ViewTransitionProtocol: AnyObject {
+    func selecteCell(id: String)
+}
