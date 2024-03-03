9623cc40d227f7e62050ee08687a0dda077a8958
chasomin <somin49@naver.com>
2024-03-03 21:09:39 +0900

    즐겨찾기 없을 때 뜰 empty view 제작

---
+++ CryptoCoinApp/Custom/EmptyView.swift
@@ -0,0 +1,40 @@
+//
+//  EmptyView.swift
+//  CryptoCoinApp
+//
+//  Created by 차소민 on 3/3/24.
+//
+
+import UIKit
+import SnapKit
+
+class EmptyView: BaseView {
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func configureHierarchy() {
+        addSubview(imageView)
+        addSubview(label)
+    }
+    
+    override func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(100)
+        }
+        label.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+        }
+    }
+    
+    override func configureView() {
+        imageView.image = .btnStar
+        label.text = "즐겨 찾는 Coin을 저장해보세요!"
+        label.font = .boldTitle
+        label.textColor = .pointColor
+        label.textAlignment = .center
+    }
+}
