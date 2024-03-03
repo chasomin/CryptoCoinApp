5946c55cf04df06511258ea04d46cb6e44bc1fb4
chasomin <somin49@naver.com>
2024-03-03 23:40:17 +0900

    즐겨찾기 한 data 10초 간격으로 호출

--- CryptoCoinApp/Favorite/FavoriteViewModel.swift
+++ CryptoCoinApp/Favorite/FavoriteViewModel.swift
@@ -13,10 +13,12 @@ final class FavoriteViewModel {
     let inputViewWillAppearTrigger: Observable<Void?> = Observable(nil)
     let inputViewDidLoadTrigger: Observable<Void?> = Observable(nil)
     let inputRefreshControl: Observable<Void?> = Observable(nil)
+//    let inputCellDragAndDrop:Observable<(String, Int)?> = Observable(nil)
     
     let outputData: Observable<[Market]> = Observable([])
     let outputError: Observable<String?> = Observable(nil)
     let outputRefresh: Observable<Void?> = Observable(nil)
+//    let outputCellDragAndDrop: Observable<Void?> = Observable(nil)
     
     init() {
         inputViewWillAppearTrigger.bind { value in
@@ -27,13 +29,9 @@ final class FavoriteViewModel {
             } else {
                 id = self.repository.fetchItem().map{$0.id}
             }
-            APIService.shared.fetchCoinMarketAPI(api: .market(id: id)) { data, error in
-                if error != nil {
-                    self.outputError.value = error!.rawValue
-                } else {
-                    guard let data else { return }
-                    self.outputData.value = data
-                }
+            self.fetch(id: id)
+            Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { _ in
+                self.fetch(id: id)
             }
         }
         inputRefreshControl.bind { value in
@@ -55,5 +53,22 @@ final class FavoriteViewModel {
                 }
             }
         }
+//        inputCellDragAndDrop.bind { value in
+//            guard let value else { return }
+//            self.repository.changeItem(value.0, index: value.1)
+//            self.outputCellDragAndDrop.value = ()
+//        }
+    }
+    
+    func fetch(id: [String]) {
+        APIService.shared.fetchCoinMarketAPI(api: .market(id: id)) { data, error in
+            if error != nil {
+                self.outputError.value = error!.rawValue
+            } else {
+                guard let data else { return }
+                self.outputData.value = data
+            }
+        }
+
     }
 }
